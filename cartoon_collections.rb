ff156c29a04c177992577dce7da2f40f0ede31a7
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-24 08:59:51 -0800

    commit tree 88ea63b932bac4712ab695c181116b4fe8212d04

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+def roll_call_dwarves(dwarf_array)# code an argument here
   # Your code here
+  counter = 1
+  dwarf_array.each do |dwarf| 
+    puts "#{counter}. #{dwarf}"
+    counter += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map { |call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(call_array)# code an argument here
   # Your code here
+  found = call_array.find { |call| call.length > 4}
+  found ? true : false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(snack_array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find { |cheese| snack_array.include?(cheese) }
 end
